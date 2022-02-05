446fee310bf90304cc989966083ffaa53639069d
core.pager=cat <yacinebabour76@gmail.com>
2022-02-05 17:59:11 +0100

    added routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+var sports =[
+  {name:"football"},
+  {name:"basketball"},
+  {name:"handball"},
+  {name:"tenis"},
+];
+var players =[
+{name:"Nadal"}
+
+
+]
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send("Welcom To Wivaplay");
+});
+/* GET home page. */
+router.get('/sports', function(req, res, next) {
+  res.send(sports);
+});
+router.get('/players', function(req,res, next ) {
+  res.send(players);
+
 });
 
 module.exports = router;
